b1e9bd0f317c4fd8b48c7571accf26d67af33039
thiag <thiag@DESKTOP-39T92QH>
2023-05-20 13:25:33 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/ContaBanco.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuSuperBanco
+{
+    public class ContaBanco
+    {
+        public string Numero { get; }
+        public string Dono { get; set; }
+        public decimal Saldo { 
+            get
+            {
+                decimal saldo = 0;
+                foreach (var item in todasTransacoes) 
+                {
+                    saldo += item.Valor;               
+                }
+                return saldo;
+            } 
+        }
+
+        public static int numeroConta = 2124214;
+        
+        private List<Transacao> todasTransacoes = new List<Transacao>();
+
+        public ContaBanco( string nome, decimal valor) 
+        {
+            this.Dono = nome;
+            numeroConta++;
+
+            this.Numero = numeroConta.ToString();
+            Depositar(valor, DateTime.Now, "Saldo inicial");
+
+        }
+
+        public void Depositar(decimal valor, DateTime data, string obs )
+        {
+            if ( valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor),"Nao aceitamos deposito negativo");
+            }
+            Transacao trans = new Transacao(valor, data, obs);
+            todasTransacoes.Add(trans);
+        }
+        public void Sacar(decimal valor, DateTime data, string obs) 
+        {
+            if ( valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "Nao aceitamos saque de valor negativo");
+            }
+            if(Saldo - valor < 0) 
+            {
+                throw new InvalidOperationException("Operacao nao é permitida");
+            }
+
+            Transacao trans = new Transacao(-valor, data, obs);
+            todasTransacoes.Add(trans);
+        }
+
+        public string PegarMovimentacao() 
+        {
+            var movimentacoes = new StringBuilder();
+
+            movimentacoes.AppendLine("Data\tValor\tObs");
+
+            foreach (var item in todasTransacoes) 
+            {
+                movimentacoes.AppendLine($"{item.Data.ToShortDateString()}\t {item.Valor}\t {item.Obs}");
+            }
+            return movimentacoes.ToString();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MeuSuperBanco
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            MeuSuperBanco.ContaBanco contaB = new ContaBanco("Thiago", 2000);
+            Console.WriteLine($"Nome: {contaB.Dono} Saldo: {contaB.Saldo} numero da conta: {contaB.Numero}");
+
+            Console.WriteLine($"O saldo é {contaB.Saldo}");
+
+            contaB.Sacar(1000, DateTime.Now, "Saquei para comprar carro");
+
+            contaB.Sacar(100, DateTime.Now, "Saque aluguel");
+
+            contaB.Sacar(200, DateTime.Now, "Saque");
+
+            try
+            {
+                contaB.Sacar(1000, DateTime.Now, "Saquei para comida");
+                Console.WriteLine($"A conta esta com {contaB.Saldo}");
+
+            }
+            catch (ArgumentOutOfRangeException ex) 
+            {
+                Console.WriteLine(ex.Message);
+
+            }catch (InvalidOperationException ex) 
+            {
+                Console.WriteLine(ex.Message);
+
+            }catch(Exception ) 
+            {
+                Console.WriteLine("Operacao inválida");
+            }
+  
+            Console.WriteLine(contaB.PegarMovimentacao());
+        }
+    }
+}
